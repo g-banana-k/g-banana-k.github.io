ede8ce0177d8e214749545a678392ba665cc9078
g_banana_k <100505456+BananaHexagon@users.noreply.github.com>
2024-11-24 11:36:57 +0900

    #

--- ssg/template/footer.ts
+++ ssg/template/footer.ts
@@ -15,6 +15,7 @@ export const footer = () => {
             <div class="footer_content">
                 <h4><a href="/info/works">Works</a></h4>
                 <ul>
+                    <li><a href="https://g-banana-k.github.io/note">Blog</a></li>
                     <li><a href="https://g-banana-k.github.io/banahexel_site">BanaHexel</a></li>
                     <li><a href="https://g-banana-k.github.io/creation">HKHUVASV</a></li>
                 </ul>
